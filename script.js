e901ec2f4f5c80178b7cef5086ac118eecb884d2
Mauricio Zavala <mauricio.zavalagr@ext.cemex.com>
2025-08-11 18:54:25 -0600

    script and style files deleted

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scrolling for nav links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
